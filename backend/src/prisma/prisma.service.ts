2f7ff05e56b692fd4f0ac024a81ac485bde1b503
Facundo-Leiva <facundoleiva1@hotmail.com>
2026-06-12 23:26:38 -0300

    Fix_deploy

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 // Servicio de la API: relacionado con el acceso a la base de datos mediante Prisma
@@ -7,12 +7,5 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     // Función: abrir conexión al iniciar el módulo
     async onModuleInit() {
         await this.$connect();
-    }
-
-    // Función: cerrar conexión al cerrar la App
-    async enableShutdownHooks(app: INestApplication) {
-        this.$on('beforeExit', async () => {
-            await app.close();
-        });
     }
 }
